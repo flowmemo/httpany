740dcd487441cd349682bcb5448d552e17963e9e
flowmemo <flowmemo@outlook.com>
2016-06-06 11:03:01 +0800

    fix license, delete redundant files

--- src/simpleserver.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-// Create an HTTP server
-var srv = http.createServer( (req, res) => {
-  res.writeHead(345, {'Content-Type': 'text/plain'});
-  res.end('okay');
-});
-
-srv.listen(3000)
